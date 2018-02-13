fb47a17aa3a4cddd8dcc5220c0e9482d6f47dd63
okprogrammmer <omk7912@gmail.com>
2018-02-14 01:08:11 +0530

    Add all files

---
+++ appTwo/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+#from appTwo.models import User
+from appTwo.forms import NewUserForm
+# Create your views here.
+
+
+def index(request):
+    return render(request,'appTwo/index.html')
+
+def users(request):
+
+    form = NewUserForm()
+
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit = True)
+            return index(request)
+        else:
+            print('Error! you have filled the form wrong!')
+
+    return render(request,'appTwo/user.html',{'form':form})
+
+
+        
+
+    
+
+
+    
+
+    
+    
